3d390f6693155b19f6f7340444cea74bf96a4ff4
Lance Hart <me@lancehart.ca>
2024-06-29 18:40:17 -0400

    Allowed callbacks to be optional

--- src/functions/bind.rs
+++ src/functions/bind.rs
@@ -8,14 +8,14 @@ extern "C" {
         i: c_int,
         blob: *const c_void,
         n: c_int,
-        destructor: extern "C" fn(*mut c_void),
+        destructor: Option<extern "C" fn(*mut c_void)>,
     ) -> c_int;
     pub fn sqlite3_bind_blob64(
         stmt: *mut SQLite3Stmt,
         i: c_int,
         blob: *const c_void,
         n: SQLite3UInt64,
-        destructor: extern "C" fn(*mut c_void),
+        destructor: Option<extern "C" fn(*mut c_void)>,
     ) -> c_int;
     pub fn sqlite3_bind_double(stmt: *mut SQLite3Stmt, i: c_int, val: c_double) -> c_int;
     pub fn sqlite3_bind_int(stmt: *mut SQLite3Stmt, i: c_int, val: c_int) -> c_int;
@@ -26,28 +26,28 @@ extern "C" {
         i: c_int,
         text: *const c_char,
         n: c_int,
-        destructor: extern "C" fn(*mut c_void),
+        destructor: Option<extern "C" fn(*mut c_void)>,
     ) -> c_int;
     pub fn sqlite3_bind_text16(
         stmt: *mut SQLite3Stmt,
         i: c_int,
         text: *const c_void,
         n: c_int,
-        destructor: extern "C" fn(*mut c_void),
+        destructor: Option<extern "C" fn(*mut c_void)>,
     ) -> c_int;
     pub fn sqlite3_bind_text64(
         stmt: *mut SQLite3Stmt,
         i: c_int,
         text: *const c_char,
         n: SQLite3Int64,
-        destructor: extern "C" fn(*mut c_void),
+        destructor: Option<extern "C" fn(*mut c_void)>,
     ) -> c_int;
     pub fn sqlite3_bind_pointer(
         stmt: *mut SQLite3Stmt,
         i: c_int,
         ptr: *mut c_void,
         r#type: *const c_char,
-        destructor: extern "C" fn(*mut c_void),
+        destructor: Option<extern "C" fn(*mut c_void)>,
     ) -> c_int;
     pub fn sqlite3_bind_zeroblob(stmt: *mut SQLite3Stmt, i: c_int, n: c_int) -> c_int;
     pub fn sqlite3_bind_zeroblob64(stmt: *mut SQLite3Stmt, i: c_int, n: SQLite3UInt64) -> c_int;
--- src/functions/exec.rs
+++ src/functions/exec.rs
@@ -6,7 +6,7 @@ extern "C" {
     pub fn sqlite3_exec(
         db: *mut SQLite3,
         sql: *const c_char,
-        callback: extern "C" fn(*mut c_void, c_int, *mut *mut c_char, *mut *mut c_char),
+        callback: Option<extern "C" fn(*mut c_void, c_int, *mut *mut c_char, *mut *mut c_char)>,
         callback_arg: *mut c_void,
         errmsg: *mut *mut c_char,
     ) -> c_int;
