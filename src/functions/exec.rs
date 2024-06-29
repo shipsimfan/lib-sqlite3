f07414e0fe1fd8c41e6c7a0fc1512eb6f6a69ba7
Lance Hart <me@lancehart.ca>
2024-06-29 18:35:52 -0400

    Added sqlite3_exec

---
+++ src/functions/exec.rs
@@ -0,0 +1,13 @@
+use crate::SQLite3;
+use std::ffi::{c_char, c_int, c_void};
+
+#[link(name = "sqlite3")]
+extern "C" {
+    pub fn sqlite3_exec(
+        db: *mut SQLite3,
+        sql: *const c_char,
+        callback: extern "C" fn(*mut c_void, c_int, *mut *mut c_char, *mut *mut c_char),
+        callback_arg: *mut c_void,
+        errmsg: *mut *mut c_char,
+    ) -> c_int;
+}
