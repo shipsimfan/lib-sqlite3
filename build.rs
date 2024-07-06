43e7764d0a7aa283ad3e08f73d862f3e6ca2c2c3
Lance Hart <me@lancehart.ca>
2024-07-06 14:52:22 -0400

    Trying to fix build script

--- build.rs
+++ build.rs
@@ -3,7 +3,7 @@ const LIBRARY_NAME: &str = "sqlite3";
 
 fn main() {
     // Inform rust to only re-run if the source file changes
-    println!("cargo::rerun-if-changed={}", SOURCE);
+    println!("cargo:rerun-if-changed={}", SOURCE);
 
     // Compile the source file
     cc::Build::new().file(SOURCE).compile(LIBRARY_NAME);
